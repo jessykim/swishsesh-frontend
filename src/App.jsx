422f8b883d5563eacbf8a6af8705242c6bec8138
jessikim <jessica.kim17@gmail.com>
2023-01-05 18:14:15 -0600

    add jwt decode functionality and sign out button/feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // import logo from './logo.svg';
 
 // npm modules
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 // import { Routes, Route, useNavigate } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
 
 // page components
 
@@ -15,9 +16,19 @@ import './App.css';
 
 
 function App() {
+  const [user, setUser] = useState({})
 
   function handleCallbackResponse (response) {
-    console.log("Encoded JWT ID token: " + response) 
+    console.log("Encoded JWT ID token: " + response.credential) 
+    const userObject = jwt_decode(response.credential)
+    console.log(userObject)
+    setUser(userObject)
+    document.getElementById("signInDiv").hidden = true
+  }
+
+  function handleSignOut(event) {
+    setUser({})
+    document.getElementById("signInDiv").hidden = false
   }
 
   useEffect(() => {
@@ -31,6 +42,8 @@ function App() {
       document.getElementById('signInDiv'),
       { theme: "outline", size: "large"}
     )
+
+    google.accounts.id.prompt()
   }, [])
 
   return (
@@ -41,6 +54,16 @@ function App() {
         <header className="App-header">
           <h1>Swishsesh Google Login!</h1>
           <div id="signInDiv"></div>
+          { Object.keys(user).length !== 0 &&
+            <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+          }
+          { user.name ?
+            <div>
+              <img src={user.picture} alt="profile"></img>
+              <h3>{user.name}</h3>
+            </div>
+            : <h1>Login!</h1>
+          }
         </header>
       </div>
     </>
